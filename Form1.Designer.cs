b1b51bb1bd1b429b174ec8fb7507e05f00e85299
Nick <93144005+NiRGEsFY@users.noreply.github.com>
2024-01-17 22:44:10 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace NastyLaba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.button1.Location = new System.Drawing.Point(370, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(12, 12);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(776, 397);
+            this.cartesianChart1.TabIndex = 1;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace NastyLaba
+{
+    public partial class Form1 : Form
+    {
+        List<Data> datas;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Algoritm
+        {
+            public void FirstEx(int n)
+            {
+                Random rand = new Random();
+                int[] array = new int[n];
+                ulong even = 0;
+                ulong notEven = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    array[i] = rand.Next(10000000, 50000000);
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    if (array[i]%2 == 0)
+                    {
+                        even += (ulong)array[i];
+                    }
+                    else
+                    {
+                        notEven += (ulong)array[i];
+                    }
+                }
+            }
+        }
+        public class Data
+        {
+            public int values { get; set; }
+            public double time { get; set; }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            datas = new List<Data>();
+            Algoritm alg = new Algoritm();
+            for (int i = 10000000; i < 50000000; i += 5000000)
+            {
+                var startTime = System.Diagnostics.Stopwatch.StartNew();
+                alg.FirstEx(i);
+                startTime.Stop();
+                datas.Add(new Data { values = i, time = startTime.ElapsedMilliseconds });
+            }
+
+            SeriesCollection series = new SeriesCollection();
+            ChartValues<int> zp = new ChartValues<int>();
+            List<string> date = new List<string>();
+            foreach (var item in datas)
+            {
+                zp.Add((int)item.time);
+                date.Add(item.values.ToString());
+            }
+            cartesianChart1.AxisX.Clear();
+            cartesianChart1.AxisX.Add(new Axis
+            {
+                Title = "Количество",
+                Labels = date
+            });
+
+            LineSeries line = new LineSeries();
+            line.Title = "";
+            line.Values = zp;
+
+            series.Add(line);
+            cartesianChart1.Series = series;
+        }
+    }
+}
